578ca3fe59ed2ba4bf5d767985abb30b56474d6c
Amartya Yadav <iamartyayadav@gmail.com>
2023-04-21 18:11:42 +0530

    restoration

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:pirate_hunt/screens/login_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:pirate_hunt/screens/signup_screen.dart';
+
+import 'screens/drop_screen.dart';
+import 'screens/home_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,8 +24,30 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.red,
+      ), 
+      
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, userSnapshot) {
+          if (userSnapshot.connectionState == ConnectionState.waiting) {
+            return const DropScreen();
+          }
+
+          if (userSnapshot.hasData) {
+            return const HomeScreen();
+          }
+          else {
+            return const LoginScreen();
+          }
+        },
       ),
-      home: const LoginScreen(),
+      
+      routes: {
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        SignUpScreen.routeName:(context) => const SignUpScreen(),
+        HomeScreen.routeName: (context) => const HomeScreen(),
+        DropScreen.routeName: (context) => const DropScreen(),
+      },
     );
   }
 }
